a6ced737995df00ba1ef640a2b6c37d30ea5ecf2
Thao <ngovanthao1997ylnd@gmail.com>
2022-01-19 13:52:39 +0700

    frontend about

---
+++ ShopThoiTrang/Controllers/AboutCustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopThoiTrang.Controllers
+{
+    public class AboutCustomerController : Controller
+    {
+        // GET: AboutCustomer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
